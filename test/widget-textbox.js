085ea589865b1e57ee727c29dfb7349316a8a0d8
Michael Camilleri <mike@inqk.net>
2018-04-05 16:03:29 +0900

    Add textbox widget (just a 1-line high textarea widget at this point).

---
+++ test/widget-textbox.js
@@ -0,0 +1,38 @@
+var blessed = require('blessed')
+  , texty = require('../')
+  , screen;
+
+screen = blessed.screen({
+  dump: __dirname + '/../logs/textbox.log',
+  fullUnicode: true,
+  warnings: true
+});
+
+var box = texty.textarea({
+  parent: screen,
+  // Possibly support:
+  // align: 'center',
+  style: {
+    bg: 'blue',
+    fg: 'white'
+  },
+  height: 1,
+  width: 5,
+  top: 'center',
+  left: 'center',
+  tags: true
+});
+
+screen.render();
+
+screen.key('q', function() {
+  screen.destroy();
+});
+
+screen.key('i', function() {
+  box.readInput(function() {});
+});
+
+screen.key('e', function() {
+  box.readEditor(function() {});
+});
